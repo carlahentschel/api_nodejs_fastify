de59f97def1e02564d044b1038f2ff53a6fdc7cb
Bowie <carlarhentschel@gmail.com>
2024-01-17 13:49:40 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+/* import { createServer } from 'node:http'
+
+const server = createServer( (request, response) => {
+
+    response.write('hello, world!');
+    return response.end();
+});
+
+server.listen(3333); */
+
+import { fastify } from 'fastify';
+
+const server = fastify();
+
+server.get('/', () => {
+    return 'Hello, world!'
+});
+
+server.get('/node', () => {
+    return 'Hello, Node!'
+});
+
+
+server.listen({
+    port: 3333,
+})
