b6894cccd4837876b2f4beb75844ae8b4ac5c4e7
Bowie <carlarhentschel@gmail.com>
2024-01-17 15:05:34 -0300

    altera métodos

--- server.js
+++ server.js
@@ -9,15 +9,49 @@ const server = createServer( (request, response) => {
 server.listen(3333); */
 
 import { fastify } from 'fastify';
+import { DatabaseMemory } from './database-memory.js';
 
 const server = fastify();
 
-server.get('/', () => {
-    return 'Hello, world!'
+const database = new DatabaseMemory();
+
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body;
+
+    database.create({
+        title: title,
+        description: description,
+        duration: duration,
+    });
+
+    return reply.status(201).send;
 });
 
-server.get('/node', () => {
-    return 'Hello, Node!'
+server.get('/videos', () => {
+    const videos = database.list();
+
+    return videos;
+});
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body;
+
+    database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send();
+});
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+
+    database.delete(videoId);
+
+    return reply.status(204).send;
 });
 
 
